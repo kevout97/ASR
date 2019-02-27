71673184ccc567105cc48065431ba4cca0074b0c
kevout <kev.gomez97@hotmail.com>
2019-02-26 18:53:45 -0600

    Se agregaron los oid de cada objeto de la MIB

--- Proyecto1/ThreadSNMP.py
+++ Proyecto1/ThreadSNMP.py
@@ -7,38 +7,40 @@ from SNMP import SNMP
 class ThreadSNMP:
     thread
 
-    def monitoring(self,ip,mainPojo,mainDBRRD):
+    def monitoring(self,ip,mainPojo,mainDBRRD,index):
         snmp = SNMP()
         hostname = self.mainPojo.getHostname(str(ip))
         community = self.mainPojo.getCommunity(str(hostname))
         version_snmp = 0 if str(self.pojo.getVersionSNMP(str(hostname))) == "v1" else 1
         port_snmp = int(self.pojo.getPortSNMP(str(hostname)))
         while True:
-            in_network_interface = snmp.get(community,version_snmp,ip,port_snmp,"")
-            out_network_interface = snmp.get(community,version_snmp,ip,port_snmp,"")
-            in_icmp = snmp.get(community,version_snmp,ip,port_snmp,"")
-            out_icmp = snmp.get(community,version_snmp,ip,port_snmp,"")
-            in_tcp = snmp.get(community,version_snmp,ip,port_snmp,"")
-            out_tcp = snmp.get(community,version_snmp,ip,port_snmp,"")
-            in_udp = snmp.get(community,version_snmp,ip,port_snmp,"")
-            out_udp = snmp.get(community,version_snmp,ip,port_snmp,"")
-            in_ping = snmp.get(community,version_snmp,ip,port_snmp,"")
-            out_ping = snmp.get(community,version_snmp,ip,port_snmp,"")
+            in_network_interface = snmp.get(community,version_snmp,ip,port_snmp,"1.3.6.1.2.1.2.2.1.10."+ str(index))
+            out_network_interface = snmp.get(community,version_snmp,ip,port_snmp,"1.3.6.1.2.1.2.2.1.16."+ str(index))
+            in_icmp = snmp.get(community,version_snmp,ip,port_snmp,"1.3.6.1.2.1.5.1.0")
+            out_icmp = snmp.get(community,version_snmp,ip,port_snmp,"1.3.6.1.2.1.5.14.0")
+            in_tcp = snmp.get(community,version_snmp,ip,port_snmp,"1.3.6.1.2.1.6.10.0")
+            out_tcp = snmp.get(community,version_snmp,ip,port_snmp,"1.3.6.1.2.1.6.11.0")
+            in_udp = snmp.get(community,version_snmp,ip,port_snmp,"1.3.6.1.2.1.7.1.0")
+            out_udp = snmp.get(community,version_snmp,ip,port_snmp,"1.3.6.1.2.1.7.4.0")
+            in_ping = int(snmp.get(community,version_snmp,ip,port_snmp,"1.3.6.1.2.1.5.8.0")) #The number of ICMP Echo (request) messages received.
+            in_ping += int(snmp.get(community,version_snmp,ip,port_snmp,"1.3.6.1.2.1.5.9.0")) #The number of ICMP Echo Reply messages received.
+            out_ping = int(snmp.get(community,version_snmp,ip,port_snmp,"1.3.6.1.2.1.5.21.0")) #The number of ICMP Echo (request) messages sent.
+            out_ping += int(snmp.get(community,version_snmp,ip,port_snmp,"1.3.6.1.2.1.5.22.0")) #The number of ICMP Echo Reply messages sent.
             mainDBRRD.insert(in_network_interface,out_network_interface,in_icmp,out_icmp,in_tcp,out_tcp,in_udp,out_udp,in_ping,out_ping)
             time.sleep(1)
 
-    def startMonitoring(self,ip,mainPojo):
+    def startMonitoring(self,ip,mainPojo,index):
         if os.path.isfile(ip + ".rrd"):
             self.dbrrd = DataBaseRRDTOOL(ip + ".rrd")
             nameThread = ip + "thread"
-            self.thread = threading.Thread(target=self.monitoring, name=nameThread,args=(ip,mainPojo,self.dbrrd,))
+            self.thread = threading.Thread(target=self.monitoring, name=nameThread,args=(ip,mainPojo,self.dbrrd,index,))
             self.thread.setDaemon(True)
             self.thread.start()
         else:
             self.dbrrd = DataBaseRRDTOOL(ip + ".rrd")
             self.dbrrd.create()
             nameThread = ip + "thread"
-            self.thread = threading.Thread(target=self.monitoring, name=nameThread,args=(ip,mainPojo,self.dbrrd,))
+            self.thread = threading.Thread(target=self.monitoring, name=nameThread,args=(ip,mainPojo,self.dbrrd,index,))
             self.thread.setDaemon(True)
             self.thread.start()
         return self.thread

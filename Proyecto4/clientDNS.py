8f7f8ce12ec0b43bb2a513c47b757c0ac7da8f11
kev <kev.gomez97@hotmail.com>
2019-06-13 11:32:46 -0500

    Cliente DNS

--- Proyecto4/clientDNS.py
+++ Proyecto4/clientDNS.py
@@ -29,30 +29,30 @@ def dnsServer(address,port,domain,requests):
                 req = '.'.join(reversed(domain.split("."))) + ".in-addr.arpa"
                 myAnswers = myResolver.query(str(req), "PTR")
                 for rdata in myAnswers:
-                        #print("Ip: " + str(domain) + "\nDomain: " + str(rdata))
+                        print("Ip: " + str(domain) + "\nDomain: " + str(rdata))
             return time() - start_time
         else:
             start_time = time()
             for i in range(0,int(requests)):
                 myAnswers = myResolver.query(str(domain), "A")
                 for rdata in myAnswers:
-                        #print("Domain: " + str(domain) + "\nIp: " + str(rdata))
+                        print("Domain: " + str(domain) + "\nIp: " + str(rdata))
             return time() - start_time
                     
     except Exception as e: 
         print(e)
         return -1
 
-# if __name__ == '__main__':
-#     from optparse import OptionParser
-#     parser = OptionParser()
-#     parser.add_option("-a","--address", dest="address", default='localhost',
-#                     help="ADDRESS for DNS server")
-#     parser.add_option("-p", "--port",dest="port", type="int", default=53,
-#                     help="PORT for DNS server")
-#     parser.add_option("-d", "--domain", dest="domain", default='localhost',
-#                     help="Domain or IP to consult")
-#     parser.add_option("-n", "--number", dest="requests", default=1,
-#                     help="Number of requestsbbb")
-#     (options, args) = parser.parse_args()
-#     dnsServer(str(options.address), options.port, options.domain, options.requests)
+if __name__ == '__main__':
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option("-a","--address", dest="address", default='localhost',
+                    help="ADDRESS for DNS server")
+    parser.add_option("-p", "--port",dest="port", type="int", default=53,
+                    help="PORT for DNS server")
+    parser.add_option("-d", "--domain", dest="domain", default='localhost',
+                    help="Domain or IP to consult")
+    parser.add_option("-n", "--number", dest="requests", default=1,
+                    help="Number of requestsbbb")
+    (options, args) = parser.parse_args()
+    dnsServer(str(options.address), options.port, options.domain, options.requests)
